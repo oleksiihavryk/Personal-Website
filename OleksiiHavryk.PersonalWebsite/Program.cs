e31399a5ad5f5896b243faa1bfae2badcf8bca3c
oleksiihavryk <oleksii.havryk2004@gmail.com>
2025-01-18 06:41:30 +0200

    Added supporting of database to the application.

--- OleksiiHavryk.PersonalWebsite/Program.cs
+++ OleksiiHavryk.PersonalWebsite/Program.cs
@@ -1,4 +1,10 @@
+using OleksiiHavryk.PersonalWebsite.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetDefaultConnectionString();
+builder.Services.AddDatabase(connectionString);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
