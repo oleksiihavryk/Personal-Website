3fa955ef741ba55ac218dc62c96a4115331ba582
oleksiihavryk <oleksii.havryk2004@gmail.com>
2025-01-25 12:40:00 +0200

    Added MVC, seeding, logging, person manager and development exception handling features in the application.

--- OleksiiHavryk.PersonalWebsite/Program.cs
+++ OleksiiHavryk.PersonalWebsite/Program.cs
@@ -3,10 +3,25 @@ using OleksiiHavryk.PersonalWebsite.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetDefaultConnectionString();
+var seed = builder.Configuration.GetPersonSeed();
+
+builder.Services.AddControllersWithViews(
+    mvc => mvc.EnableEndpointRouting = false);
+
+builder.Services.AddLogging();
 builder.Services.AddDatabase(connectionString);
+builder.Services.AddPersonManager();
 
-var app = builder.Build();
+var app = await builder.BuildWithSeed(seed);
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
 
-app.Run();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseExceptionHandler();
+}
+
+app.UseMvcWithDefaultRoute();
+
+await app.RunAsync();
